2d7cf3011e741f69152ae27b8b68c95c7ae4b7a1
Mai <mai@tilde.team>
2025-04-01 12:17:15 -0300

    home(nu): temporarily remove a plugin build issues, yippie

--- common/homeManagerModules/shells/nu/default.nix
+++ common/homeManagerModules/shells/nu/default.nix
@@ -3,7 +3,11 @@ let
   inherit (unstable) nushell nushellPlugins;
   plugins = import ./plugins.nix unstable;
 in {
-  home.packages = [ plugins.file ] ++ (with nushellPlugins; [ query ]);
+  home.packages =
+    # [ plugins.file ] 
+    # ++ 
+
+    (with nushellPlugins; [ query ]);
   programs.nushell = {
     enable = true;
     package = nushell;
